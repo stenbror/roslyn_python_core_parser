35e3bca99bc7238499fe498aacf40b601303f33d
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-12-28 00:33:46 +0100

    Added rule for func type statement.

--- src/parser/python_core_block_parser.rs
+++ src/parser/python_core_block_parser.rs
@@ -1,3 +1,5 @@
+use crate::parser::python_core_expression_parser::ExpressionRules;
+use crate::parser::python_core_tokenizer::LexerMethods;
 use crate::parser::syntax_error::SyntaxError;
 use crate::parser::syntax_nodes::SyntaxNode;
 use crate::parser::token_nodes::Token;
@@ -93,12 +95,44 @@ impl BlockGrammarRules for PythonCoreParser {
 
         match &*self.lexer.symbol {
             Token::EofToken( _ , _ , _ ) => Ok(Box::new(SyntaxNode::FuncTypeInputStmtNode(pos, self.lexer.position, right, nodes, self.lexer.symbol.clone()))),
-            _ => Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting EOF in type input!"))))
+            _ => Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting EOF in func type input!"))))
         }
     }
 
     fn parse_func_type(&mut self) -> Result<Box<SyntaxNode>, Box<SyntaxError>> {
-        todo!()
+        let pos = self.lexer.position;
+        match &*self.lexer.symbol {
+            Token::LeftParenToken( _ , _ , _ ) => {
+                let symbol1 = self.lexer.symbol.clone();
+                self.lexer.advance();
+
+                let left = match &*self.lexer.symbol {
+                    Token::RightParenToken( _ , _ , _ ) => None,
+                    _ => Some(self.parse_type_list()?)
+                };
+
+                match &*self.lexer.symbol {
+                    Token::RightParenToken( _ , _ , _ ) => {
+                        let symbol2 = self.lexer.symbol.clone();
+                        self.lexer.advance();
+
+                        match &*self.lexer.symbol {
+                            Token::ArrowToken( _ , _ , _ ) => {
+                                let symbol3 = self.lexer.symbol.clone();
+                                self.lexer.advance();
+
+                                let right = self.parse_test_expr()?;
+
+                                Ok(Box::new(SyntaxNode::FuncTypeStmtNode(pos, self.lexer.position, symbol1, left, symbol2, symbol3, right)))
+                            },
+                            _ => Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting '->' in func type input!"))))
+                        }
+                    },
+                    _ => Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting ')' in func type input!"))))
+                }
+            },
+            _ => Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting '(' in func type input!"))))
+        }
     }
 
     fn parse_type_list(&mut self) -> Result<Box<SyntaxNode>, Box<SyntaxError>> {
