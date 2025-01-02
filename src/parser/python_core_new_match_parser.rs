5f995ecf1aaa9128a5fe960d5bcea6d5889f99f3
Richard Magnor Stenbro <stenbror@hotmail.com>
2025-01-02 15:19:49 +0100

    Refactor and fix match statement parent rule.

--- src/parser/python_core_new_match_parser.rs
+++ src/parser/python_core_new_match_parser.rs
@@ -1,21 +1,125 @@
 use crate::parser::python_core_parser::PythonCoreParser;
+use crate::parser::python_core_tokenizer::LexerMethods;
 use crate::parser::syntax_error::SyntaxError;
 use crate::parser::syntax_nodes::SyntaxNode;
-
-
-
+use crate::parser::token_nodes::Token;
 // Refactor match parser to a LL(1) parser. New rules not based on Python grammar 3.13 but will parse equal 3.13
 
+// match_stmt:  'match' subject_expr ':' NEWLINE INDENT case_blocks+ DEDENT
+//
+// Child nodes are:   subject_expr and case_blocks
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
 
 
 pub(crate) trait MatchPatternRulesNew {
     fn parse_match_stmt_new(&mut self) -> Result<Box<SyntaxNode>, Box<SyntaxError>>;
+    fn parse_subject_expr(&mut self) -> Result<Box<SyntaxNode>, Box<SyntaxError>>;
+
+
+
+    fn parse_case_block(&mut self) -> Result<Box<SyntaxNode>, Box<SyntaxError>>;
 }
 
 impl MatchPatternRulesNew for PythonCoreParser {
     fn parse_match_stmt_new(&mut self) -> Result<Box<SyntaxNode>, Box<SyntaxError>> {
-        unimplemented!()
+        let pos = self.lexer.position;
+        let mut nodes = Vec::<Box<SyntaxNode>>::new();
+
+        let symbol1 = match &*self.lexer.symbol {
+            Token::NameToken( s, e, text, t) => {
+                match text.as_str() {
+                    "match" => Box::new(Token::MatchToken(*s, *e, t.clone())),
+                    _ => return Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting 'match' keyword in match statement!"))))
+                }
+            },
+            _ => return Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting 'match' keyword in match statement!"))))
+        };
+
+        let right = self.parse_subject_expr()?;
+
+        match &*self.lexer.symbol {
+            Token::ColonToken( _ , _ , _ ) => {
+                let symbol2 = self.lexer.symbol.clone();
+                self.lexer.advance();
+
+                let symbol3 = match &*self.lexer.symbol {
+                    Token::NewlineToken( _ , _ , _ , _ , _ ) => {
+                        let symbol10 = self.lexer.symbol.clone();
+                        self.lexer.advance();
+                        symbol10
+                    },
+                    _ => return Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting NEWLINE in 'match' statement!"))))
+                };
+
+                match &*self.lexer.symbol {
+                    Token::IndentToken( _ , _ , _ ) => {
+                        let symbol4 = self.lexer.symbol.clone();
+                        self.lexer.advance();
+
+                        /* First case must be available */
+                        match &*self.lexer.symbol {
+                            Token::NameToken( _ , _ , text, _ ) => {
+                                if text.as_str() == "case" {
+                                    nodes.push(self.parse_case_block()?)
+                                }
+                                else {
+                                    return Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting at least one 'case' keyword in 'match' statement!"))))
+                                }
+                            },
+                            _ => return Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting at least one 'case' keyword in 'match' statement!"))))
+                        }
+
+                        /* Optional rest of cases */
+                        loop {
+                            match &*self.lexer.symbol {
+                                Token::NameToken( _ , _ , text, _ ) => {
+                                    if text.as_str() == "case" {
+                                        nodes.push(self.parse_case_block()?)
+                                    }
+                                    else {
+                                        break;
+                                    }
+                                },
+                                _ => break
+                            }
+                        }
+
+                        /* Make sure we have a dedent after all case blocks. */
+                        let symbol5 = match &*self.lexer.symbol {
+                            Token::DedentToken( _ , _ , _ ) => {
+                                let symbol11 = self.lexer.symbol.clone();
+                                self.lexer.advance();
+                                symbol11
+                            },
+                            _ => return Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting DEDENT in 'match' statement!"))))
+                        };
+
+                        nodes.reverse();
+
+                        Ok(Box::new(SyntaxNode::MatchStmtNode(pos, self.lexer.position, symbol1, right, symbol2, symbol3, symbol4, nodes, symbol5)))
+                    },
+                    _ => Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting INDENT in 'match' statement!"))))
+                }
+            },
+            _ => Err(Box::new(SyntaxError::new(self.lexer.position, String::from("Expecting ':' in 'match' statement!"))))
+        }
+    }
+
+    fn parse_subject_expr(&mut self) -> Result<Box<SyntaxNode>, Box<SyntaxError>> {
+        todo!()
+    }
+
+    fn parse_case_block(&mut self) -> Result<Box<SyntaxNode>, Box<SyntaxError>> {
+        todo!()
     }
 }
